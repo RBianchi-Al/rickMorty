b729087630cf40e2939c94a14c110b56f0429438
RBianchi-Al <renatalebianchi@gmail.com>
2021-07-29 10:55:33 -0300

    first commit

---
+++ src/Pages/index.tsx
@@ -0,0 +1,15 @@
+import { Header } from "../Components/Header";
+import { List } from "../Components/List";
+import { Search } from "../Components/Search";
+
+
+export function Home(){
+    return(
+        <>
+            <Header/>
+            <Search/>
+            <List/>
+
+        </>
+    )
+}
